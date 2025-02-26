3a1dc38c278af33f890fe6409ebfced25b968caa
Davi Tavares <dxrktavares3@gmail.com>
2025-02-26 10:28:31 -0300

    Aula 02 - Concatenação de variaveis

---
+++ Aula02/concatenar.php
@@ -0,0 +1,13 @@
+<?php
+    ECHO"Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+    $nome = "Davi";
+    $sobrenome = "Tavares";
+    $numero = 21;
+
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome .  " " . $sobrenome . "</p>";
+    
+?>          
