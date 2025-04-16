8e3d884ec79f198f0e84e1c29d3c07281269f0c6
Davi Tavares <dxrktavares3@gmail.com>
2025-04-16 08:50:50 -0300

    Adiciona Aula05

---
+++ Aula05/funcao.php
@@ -0,0 +1,19 @@
+<?php
+    function myMessage() {
+        echo "Ai aii, cê sabee <br>";
+    }
+
+    myMessage();
+    myMessage();
+
+    function Soma($n1, $n2) {
+        echo $n1 + $n2, "<br>";
+    }
+
+    Soma(3, 4);
+    Soma(5, 8);
+
+    function cadastrar(){
+        //vai ser uma lógica para cadastrar
+    }
+?>
